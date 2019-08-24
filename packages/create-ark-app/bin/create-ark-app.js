b888baa598a1025fcb4afce0cde6ae90092a52a2
Levblanc <levblanc@gmail.com>
2019-08-24 19:23:17 +0800

    feat: added checkNodeVersion func

--- packages/create-ark-app/bin/create-ark-app.js
+++ packages/create-ark-app/bin/create-ark-app.js
@@ -1,7 +1,12 @@
 'use strict';
 
-module.exports = createArkApp;
+const program = require('commander')
+const packageJson = require('../package.json')
+const checkNodeVersion = require('../src/checkNodeVersion')
+// const getEnvInfo = require('../src/getEnvInfo')
+// const validateProjectName = require('../src/validateProjectName')
+// const initApp = require('../src/initApp')
 
-function createArkApp() {
-    // TODO
-}
+let appName;
+
+checkNodeVersion()

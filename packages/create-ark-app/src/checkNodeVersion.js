f9e52252f8eb38d37175dd6cadf6fefe4c857dac
Levblanc <levblanc@gmail.com>
2019-08-25 16:37:26 +0800

    chore: switch to logger in ark-dev-utils

--- packages/create-ark-app/src/checkNodeVersion.js
+++ packages/create-ark-app/src/checkNodeVersion.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const logger = require('./logger')
+const logger = require('@arkitect/ark-dev-utils/logger')
 
 module.exports = () => {
     const nodeVersion = process.versions.node

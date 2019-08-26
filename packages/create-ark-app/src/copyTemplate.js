541f82163582f18cb565c0dbaa488ca4d41fb0f4
Levblanc <levblanc@gmail.com>
2019-08-26 15:07:51 +0800

    feat: add copy template func

---
+++ packages/create-ark-app/src/copyTemplate.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const fs = require('fs-extra')
+const path = require('path')
+const logger = require('@arkitect/ark-dev-utils/logger')
+
+module.exports = async ({templatePath, appPath, appName}) => {
+  const spinner = logger.spinner()
+  spinner.start('App files initializing......')
+
+  try {
+    await fs.copy(templatePath, appPath)
+
+    // Rename gitignore file to .gitignore
+    // See:
+    // https://github.com/yeoman/generator/issues/812#issuecomment-106318970
+    // and
+    // https://github.com/npm/npm/issues/1862
+    await fs.move(
+      path.resolve(appPath, './gitignore'),
+      path.resolve(appPath, './.gitignore')
+    )
+
+    spinner.success('Init app files success!')
+  } catch (err) {
+    spinner.fail('App files init failed with error:')
+    logger.error(err)
+
+    process.exit(1)
+  }
+}
